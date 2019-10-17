fe43a55ac3d57bd9cac8425173a6ca6a1140ac75
Prithijit Das <55277163+rasoraj@users.noreply.github.com>
2019-10-17 18:21:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+TweenMax.to(".super", 2, {x:550, y:450});
+TweenMax.to(".super2", 2, {x:750, y:450});
+TweenMax.to(".super3", 2, {x:950, y:450});
+TweenMax.to(".super4", 2, {x:1150, y:450});
+TweenMax.to(".super5", 2, {x:350, y:450});
+TweenMax.to(".super6", 2, {x:150, y:450});
+
+function myfunction()
+{
+    document.getElementById("hd1").innerHTML = "Super Man!"
+}
+
+function myfunction2()
+{
+    document.getElementById("hd1").innerHTML = "Bat Man!"
+}
+
+function myfunction3()
+{
+    document.getElementById("hd1").innerHTML = "Green Lantern"
+}
+
+function myfunction4()
+{
+    document.getElementById("hd1").innerHTML = "Wonder Woman"
+}
+
+function myfunction5()
+{
+    document.getElementById("hd1").innerHTML = "Cyborg"
+}
+
+function myfunction6()
+{
+    document.getElementById("hd1").innerHTML = "Flash"
+}
+    
